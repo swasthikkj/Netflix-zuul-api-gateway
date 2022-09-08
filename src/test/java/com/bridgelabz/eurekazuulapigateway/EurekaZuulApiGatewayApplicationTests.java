7933e10d4e9fbaf2f4b35181fb4f2b9e9ebd04e3
swasthikkj <swasthikkj@gmail.com>
2022-09-08 15:41:40 +0530

    added zuul proxy gateway

---
+++ src/test/java/com/bridgelabz/eurekazuulapigateway/EurekaZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.eurekazuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
